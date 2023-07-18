0e88d795af9fc3872dc9f25076b1b0a8a17e0e0b
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2023-07-18 23:28:49 +0530

    gsap code and video sync with heading

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+function init() {
+    
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
 });
+// when Locomotive Scroll updates, ScrollTrigger to update too 
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+init()
+var tl=  gsap.timeline({
+  
+    scrollTrigger:{
+      trigger:".page1 h1",
+      scroller:".main",
+      //markers:true,
+      start:"top 27%",
+      end:"top 0",
+      scrub:3
+    }
+})
+tl.to(".page1 h1", {
+    x:-100
+},"sametime")
+tl.to(".page1 h2",{
+    x:200
+},"sametime")
+tl.to(".page1 video", {
+    width:"90%"
+},"sametime")
